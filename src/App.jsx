6dc35f5e95368d10011f339f684544459705e9ec
Kiprotichabiud <kiprotichabiud1@gmail.com>
2025-06-05 12:55:10 +0300

    append Home to display coin data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+
 import React from 'react'
-import Home from './pages/Home.jsx'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div className='bg-primary'>
-     <Home />
+    <div>
+      <Home />
     </div>
   )
 }
