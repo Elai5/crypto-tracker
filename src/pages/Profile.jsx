1d8b91a06dd8d86c0f634e3867a7477e93f3cc0f
Elai5 <yvettelaine007@gmail.com>
2025-06-28 07:40:13 +0300

    added supabaseclient file to set up supabase configuration, set up the account credetianls in the env and installed supabase

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -27,21 +27,25 @@ export const Profile = () => {
             />
           </div>
         </div>
-        <div className="flex flex-col items-center  gap-3 p-4 rounded shadow-md w-1/2 text-center">
+        <div className="flex flex-col gap-3 p-4 rounded shadow-md w-1/2 border-2">
           <div>
             {" "}
             <h2 className="font-semibold text-white text-2xl">
-              Maria Fernanda
+              Bio & Other Details
             </h2>
-            <span className="text-base text-green-400">Premium User</span>
           </div>
 
-          <div className="w-48 h-48 rounded-full overflow-hidden border-[5px] border-gray-300">
-            <img
-              src={assets.cartoon}
-              alt=""
-              className="w-full h-full object-cover"
-            />
+          <div className="">
+            <h2 className="text-gray-400">Full Name</h2>
+            <p className=" text-white">Aboiud Kiprotich</p>
+          </div>
+          <div className="">
+            <h2 className="text-gray-400">Email Address</h2>
+            <p className=" text-white">Aboiud Kiprotich</p>
+          </div>
+            <div className="">
+            <h2 className="text-gray-400">Account Type</h2>
+            <p className=" text-white">Aboiud Kiprotich</p>
           </div>
         </div>
       </div>
