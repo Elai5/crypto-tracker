034b1c719d9a5718692aff36a1b2af32bfc4bb70
Elai5 <yvettelaine007@gmail.com>
2025-06-28 07:07:30 +0300

    added padded to profile se tion to allow display of infromation

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+/** @format */
+
+import React from "react";
 
 export const Profile = () => {
+  console.log("Profile page loaded");
   return (
-    <div>
-        <div className="flex">
-            
-        </div>
+    <div className="p-6 mt-20 min-h-screen">
+      <h1 className="text-3xl font-bold mb-2">Profile</h1>
+      <p className="mb-4">View all your profile details here.</p>
+
+      <div className="bg-white p-4 rounded shadow-md">
+        <h2 className="text-xl font-semibold">Maria Fernanda</h2>
+        <span className="text-sm text-gray-500">Premium User</span>
+      </div>
     </div>
-  )
-}
+  );
+};
