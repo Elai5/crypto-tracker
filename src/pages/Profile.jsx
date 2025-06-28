c89f03284bb9a8c64ecf000e9c05f8de7c46999b
Elai5 <yvettelaine007@gmail.com>
2025-06-28 07:29:10 +0300

    added profile picture on my dashboard.dumy one

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,49 @@
 /** @format */
 
 import React from "react";
+import { assets } from "../assets/assets";
 
 export const Profile = () => {
   console.log("Profile page loaded");
   return (
-    <div className="p-6 mt-20 min-h-screen">
-      <h1 className="text-3xl font-bold mb-2">Profile</h1>
-      <p className="mb-4">View all your profile details here.</p>
+    <div className="bg-gray-900 p-6 mt-20 min-h-screen">
+      <h1 className="text-3xl font-bold mb-2 text-white">Profile</h1>
+      <p className="mb-4 text-gray-500">View all your profile details here.</p>
+      <div className="flex">
+        <div className="flex flex-col items-center  gap-3 p-4 rounded shadow-md w-1/2 text-center">
+          <div>
+            {" "}
+            <h2 className="font-semibold text-white text-2xl">
+              Maria Fernanda
+            </h2>
+            <span className="text-base text-green-400">Premium User</span>
+          </div>
 
-      <div className="bg-white p-4 rounded shadow-md">
-        <h2 className="text-xl font-semibold">Maria Fernanda</h2>
-        <span className="text-sm text-gray-500">Premium User</span>
+          <div className="w-48 h-48 rounded-full overflow-hidden border-[5px] border-gray-300">
+            <img
+              src={assets.cartoon}
+              alt=""
+              className="w-full h-full object-cover"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col items-center  gap-3 p-4 rounded shadow-md w-1/2 text-center">
+          <div>
+            {" "}
+            <h2 className="font-semibold text-white text-2xl">
+              Maria Fernanda
+            </h2>
+            <span className="text-base text-green-400">Premium User</span>
+          </div>
+
+          <div className="w-48 h-48 rounded-full overflow-hidden border-[5px] border-gray-300">
+            <img
+              src={assets.cartoon}
+              alt=""
+              className="w-full h-full object-cover"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
